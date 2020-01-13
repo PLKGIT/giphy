398a054e617d4abffaf9bf2a94550b2f96155f67
Lammie <plkelly@yahoo.com>
2020-01-12 18:51:52 -0800

    Updated code to try to fix submit button issue that blocks my onclick events.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -45,8 +45,10 @@ $(document).ready(function () {
 
 
     // Retrieve images from Giphy based on topic button clicked
+
+    
     $(".topic").on("click", function () {
-        var clicked = $(this).attr("data-name");
+        clicked = $(this).attr("data-name");
         console.log(clicked);
         var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=Nc3eXj6dFPCoPEPYadwqOusNVwHez7SN&q=" + clicked + "&limit=10&lang=en";
 
@@ -60,26 +62,58 @@ $(document).ready(function () {
         // console.log(queryURL);
 
     });
-    
+
+    // Create Buttons from Array
+    function newButtons() {
+
+        // Deleting the buttons prior to adding new buttons
+        $("#buttons").empty();
+
+        // Looping through the array of movies
+        for (var i = 0; i < arrTopics.length; i++) {
+
+            // Then dynamicaly generating buttons for each movie in the array.
+            // This code $("<button>") is all jQuery needs to create the start and end tag. (<button></button>)
+            var a = $("<button>");
+            // Adding a class
+            a.addClass("btn btn-outline-info btn-lg m-1 topic");
+            // Adding a data-attribute with a value of the movie at index i
+            a.attr("data-name", arrTopics[i]);
+            // Providing the button's text with a value of the movie at index i
+            a.text(arrTopics[i]);
+            // Adding the button to the HTML
+            $("#buttons").append(a);
+        }
+    }
+
 
 
     // New Animal Listener
-    $("#new_animal").on("click", function (event) {
+
+    $("#submit").on("click", function (event) {
         event.preventDefault();
 
-        // Append user input to array in lower case
+        // Convert user's input to lower case and assign to a variable
         var usrInput = $("#add_button").val().toLowerCase();
 
         if (usrInput === "") {
-            alert("A name is required!");
-        } else {
-            // console.log(usrInput);
+            alert("A entry is required!");
+
+        } else if (arrTopics.includes(usrInput)) {
+
+            alert("A button for this animal already exists!");
+
+        } 
+         else {
+
+            event.preventDefault();
+
+            // Append user input to array in lower case
             arrTopics.push(usrInput);
             // Reset the form field
             $("#add_button").val('');
             // Recreate buttons from the array
-            createButtons();
-
+            newButtons();
         }
 
     });
@@ -87,31 +121,33 @@ $(document).ready(function () {
 
     var createRow = function (response) {
 
+        var cntImage=0;
+
         for (i = 0; i < response.data.length; i++) {
 
             var iURL = response.data[i].images.fixed_height_still.url;
             var sURL = response.data[i].images.fixed_height_still.url;
             var aURL = response.data[i].images.fixed_height_downsampled.url;
 
-            var tRow = $("<p>");
-            var rating = "Rating: " + response.data[i].rating + "&nbsp;";
+            var iRow = $("<p>");
             var image = "<img src='" + iURL + "' data-still='" + sURL + "' data-animate='" + aURL + "' data-state='still' class='gif'>";
+            var caption = "<figcaption>Rating: " + response.data[i].rating + "</figcaption>"
 
-            // Prepend concatenated row to the table
-            tRow.append(rating, image);
-            console.log(tRow);
-            $("#images").prepend(tRow);
+            // Prepend concatenated row to the HTML
+            iRow.append(image, caption);
+            console.log(iRow);
+            $("#images").prepend(iRow);
         };
 
     }
 
-    
 
-     // Clicked Gif Listener
 
-     $(document).on("click",".gif", function(){
+    // Clicked Gif Listener
 
-    //  $(".gif").on("click", function () {
+    $(document).on("click", ".gif", function () {
+
+        //  $(".gif").on("click", function () {
         // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
         var state = $(this).attr("data-state");
         console.log(state);
